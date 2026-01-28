cdf580f50a202b32a8bf0eb2aedba2cb68f33fe4
macbook <jamaldeen.o@yahoo.com>
2026-01-28 14:56:59 +0100

    Rate limited the backend's api routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -69,7 +69,7 @@ app.use("/api/v1", profileRouter);
 
 // Game
 app.use("/games/:userId", dataLimiter);
-app.use("/api/v1", gameRouter);
+app.use("/api/v1", gameRouter)
 
 
 // Listens for a port (if redis and database are connected)
